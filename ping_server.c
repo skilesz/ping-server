8e6fea81ee01b60b645a138a3aae0af536212d78
skilesz <skilesz@purdue.edu>
2020-04-23 19:46:55 -0400

    First commit

---
+++ ping_server.c
@@ -0,0 +1,13 @@
+/*
+Application that sends an echo_request to specified server, then waits for an echo_reply in order to determine
+RTT (min, max, and avg) and the loss percentage based on the number of packets sent and received.
+
+Programmed by Zach Skiles, skilesz@purdue.edu
+Last Updated: 4-22-2020 10:00 am
+*/
+
+#include "ping_server.h"
+
+int main(int argc, char *argv[]) {
+  return 0;
+} //main()
---
+++ ping_server.h
@@ -0,0 +1,30 @@
+/*
+HEADER FILE FOR ping_server.c
+
+Programmed by Zach Skiles, skilesz@purdue.edu
+Last Update: 4-23-2020 7:41 pm
+*/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/ip_icmp.h>
+#include <time.h>
+#include <fcntl.h>
+#include <signal.h>
+
+typedef struct pack {
+  struct icmphdr header;
+  char data[PING_PKT_S - sizeof(struct icmphdr)];
+} packet;
+
+int checksum(void *bin, int len);
+char *dns_lookup(char *dest_name, struct sockaddr_in *addr);
+char *reverse_lookup(char *dest_ip);
+void ping(int master_socket, struct sockaddr_in *addr, char *name, char *ip_addr, char *input);
