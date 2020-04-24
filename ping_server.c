c7219ad490b259c5e5cf6cbd05f9fd427a348420
skilesz <skilesz@purdue.edu>
2020-04-23 22:07:13 -0400

    New commit

--- ping_server.c
+++ ping_server.c
@@ -110,7 +110,7 @@ int main(int argc, char *argv[]) {
   //Socket setup
   master_socket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
   if (master_socket < 0) {
-    perror();
+    perror("Socket error\n");
     printf("Failed to create socket.\n");
     return 0;
   }
